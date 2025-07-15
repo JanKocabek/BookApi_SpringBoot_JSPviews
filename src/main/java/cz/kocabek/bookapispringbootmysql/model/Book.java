c618ff5a10c25c0fbef5651ad6ec56208cfcc3db
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-15 10:48:25 +0200

    changing Book POJO into entity and apply LoomBook annotation

--- src/main/java/cz/kocabek/bookapispringbootmysql/model/Book.java
+++ src/main/java/cz/kocabek/bookapispringbootmysql/model/Book.java
@@ -2,13 +2,25 @@ package cz.kocabek.bookapispringbootmysql.model;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import cz.kocabek.bookapispringbootmysql.dto.View;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @JsonView(View.Book.class)
+    @Id
+    @GeneratedValue
     private Long id;
     @JsonView(View.Book.class)
     @NotNull(message = "Book must have ISBN")
@@ -22,98 +34,5 @@ public class Book {
     private String publisher;
     @JsonView(View.Book.class)
     private String type;
-
-
-    public Book() {
-
-    }
-
-    public Book(Long id, String isbn, String title, String author, String publisher, String type) {
-        this.id = id;
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.type = type;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-               "id=" + id +
-               ", isbn='" + isbn + '\'' +
-               ", title='" + title + '\'' +
-               ", author='" + author + '\'' +
-               ", publisher='" + publisher + '\'' +
-               ", type='" + type + '\'' +
-               '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Book book = (Book) o;
-        return getId().equals(book.getId()) && Objects.equals(getIsbn(), book.getIsbn()) && Objects.equals(getTitle(), book.getTitle()) && Objects.equals(getAuthor(), book.getAuthor()) && Objects.equals(getPublisher(), book.getPublisher()) && Objects.equals(getType(), book.getType());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getId().hashCode();
-        result = 31 * result + Objects.hashCode(getIsbn());
-        result = 31 * result + Objects.hashCode(getTitle());
-        result = 31 * result + Objects.hashCode(getAuthor());
-        result = 31 * result + Objects.hashCode(getPublisher());
-        result = 31 * result + Objects.hashCode(getType());
-        return result;
-    }
 
 }
