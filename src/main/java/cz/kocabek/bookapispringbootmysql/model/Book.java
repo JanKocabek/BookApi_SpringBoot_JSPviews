5c192e37204c27adaf633e3386544bd4b42933ea
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-18 21:44:15 +0200

    preparing for auto fix by hibernate for db setting proper column values setting valid constrains

--- src/main/java/cz/kocabek/bookapispringbootmysql/model/Book.java
+++ src/main/java/cz/kocabek/bookapispringbootmysql/model/Book.java
@@ -3,8 +3,9 @@ package cz.kocabek.bookapispringbootmysql.model;
 import com.fasterxml.jackson.annotation.JsonView;
 import cz.kocabek.bookapispringbootmysql.api.dto.View;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,25 +18,29 @@ import lombok.NoArgsConstructor;
 @Table(name = "books")
 @JsonView(View.Book.class)
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    @NotNull(message = "Book must have ISBN")
-    @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$", message = "ISBN must be 10  or 13 digits long and can contain only digits and dashes")
+    @NotEmpty(message = "Book must have ISBN")
+    @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$", message = "ISBN can contain only  10 or 13  digits, and dashes")
+    @Size(min = 10, max = 20, message = "ISBN must be between 10 to 20 characters")
+    @Column(unique = true, length = 20, nullable = false)
     private String isbn;
 
-
+    @NotEmpty(message = "Book must have title")
     private String title;
 
-
+    @NotEmpty(message = "Book must have author")
+    @Column(nullable = false)
     private String author;
 
-
+    @NotEmpty(message = "Book must have publisher")
+    @Column(nullable = false)
     private String publisher;
 
+    @NotEmpty(message = "Book must have type")
+    @Column(nullable = false)
     private String type;
 
 
